40f7825dc65a33baa1c6e0c03c7b79f142306ae8
Kalyani Chawak <kchawak@pdx.edu>
2017-08-24 16:00:31 -0700

    Add files via upload

--- RANSAC.py
+++ RANSAC.py
@@ -15,6 +15,18 @@ pcl.save(cloud_filtered, filename)
 
 
 # PassThrough filter
+# Creating the filter
+passthrough = cloud_filtered.make_passthrough_filter()
+# Assigning axis and range to the filter object
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+# Use the filter function to obtain resultant point
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 
 # RANSAC plane segmentation
