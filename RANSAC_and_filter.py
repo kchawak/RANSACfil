ab52cd58555e5724959b8aa17918d14b0257fc0c
Kalyani Chawak <kchawak@pdx.edu>
2017-08-27 16:23:37 -0700

    Add files via upload

--- RANSAC_and_filter.py
+++ RANSAC_and_filter.py
@@ -9,23 +9,67 @@ from pcl_helper import *
 def pcl_callback(pcl_msg):
 
     # TODO: Convert ROS msg to PCL data
+    cloud = ros_to_pcl(pcl_msg)
 
     # TODO: Voxel Grid Downsampling
+    vox = cloud.make_voxel_grid_filter()
+    LEAF_SIZE = 0.01
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    cloud_filtered = vox.filter()
+    filename = 'voxel_downsampled.pcd'
+    pcl.save(cloud_filtered, filename)
 
     # TODO: PassThrough Filter
-
+    # Creating the filter
+    passthrough = cloud_filtered.make_passthrough_filter()
+    # Assigning axis and range to the filter object
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0.6
+    axis_max = 1.1
+    passthrough.set_filter_limits(axis_min, axis_max)
+    # Use the filter function to obtain resultant point
+    cloud_filtered = passthrough.filter()
+    filename = 'pass_through_filtered.pcd'
+    pcl.save(cloud_filtered, filename)
 
     # TODO: RANSAC Plane Segmentation
+    # Creating object
+    seg = cloud_filtered.make_segmenter()
+    # Setting model
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    # Setting max_distance for segmenting the table
+    max_distance = 0.01
+    seg.set_distance_threshold(max_distance)
+    # Call segment function to obtain inlier indices and model coefficients
+    inliers, coefficients = seg.segment()
 
     # TODO: Extract inliers and outliers
+    extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+    filename = 'extracted_inliers.pcd'
+    pcl.save(extracted_inliers, filename)
+    extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+    filename = 'extracted_outliers.pcd'
+    pcl.save(extracted_outliers, filename)
+    # Create the outlier filter
+    outlier_filter = cloud_filtered.make_statistical_outlier_filter()
+    # Set the number of neighboring points
+    outlier_filter.set_mean_k(50)
+    # Set threshold scale factor
+    x = 1.0
+    # Point with mean distance larger than global is outlier
+    outlier_filter.set_std_dev_mul_thresh(x)
+    # Calling filter
+    cloud_filtered = outlier_filter.filter()
 
     # TODO: Euclidean Clustering
 
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
 
     # TODO: Convert PCL data to ROS messages
-    ros_cloud_objects = 
-    ros_cloud_table = 
+    ros_cloud_objects = pcl_to_ros(extracted_outliers)
+    ros_cloud_table = pcl_to_ros(extracted_inliers)
 	
    # TODO: Publish ROS messages
     pcl_objects_pub.publish(ros_cloud_objects)
